2671fa1e16893f9ff6496d28c1282b66afe94be0
Kim Bjerge <au288681@uni.au.dk>
2023-10-25 20:56:14 +0200

    Added training scripts

---
+++ trainAll.sh
@@ -0,0 +1,6 @@
+#python FewShotTraining.py --model resnet18 --dataset CUB --mode classic --epochs 10
+#python FewShotTraining.py --model resnet34 --dataset CUB --mode classic --epochs 10
+#python FewShotTraining.py --model resnet50 --dataset CUB --mode classic --epochs 10
+python FewShotTraining.py --model resnet18 --dataset euMoths --mode classic --epochs 10
+python FewShotTraining.py --model resnet34 --dataset euMoths --mode classic --epochs 10
+python FewShotTraining.py --model resnet50 --dataset euMoths --mode classic --epochs 10
---
+++ trainCUBwithout.sh
@@ -0,0 +1,3 @@
+python FewShotWithoutPretraining.py --model resnet18 --dataset CUB --mode episodic --epochs 1700
+python FewShotWithoutPretraining.py --model resnet34 --dataset CUB --mode episodic --epochs 1700
+python FewShotWithoutPretraining.py --model resnet50 --dataset CUB --mode episodic --epochs 1700
