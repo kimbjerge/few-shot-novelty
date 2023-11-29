5ae9b052b0e644141ab385e4667a8f7450796d22
Kim Bjerge <au288681@uni.au.dk>
2023-11-29 10:27:52 +0100

    Added plotting results for EU moths of scores (Accuracy, Precision, Recall, F1) vs. N-way (Classes)

--- PlotNoveltyResultsAdv.py
+++ PlotNoveltyResultsAdv.py
@@ -9,19 +9,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-#%% MAIN
-if __name__=='__main__':
+def plotScorsVsAlpha(data_df, fewShotClassifier, title, limits, n_shot=5):
     
-    
-    n_shot = 5
-    fewShotClassifier = "Prototypical"
-    #fewShotClassifier = "BD-CSPN"
-    #data_df = pd.read_csv("./modelsOmniglotAdvStd1/resnet12_Omniglot_novelty_test.txt")
-    #data_df = pd.read_csv("./modelsOmniglotAdvStd3/resnet12_Omniglot_novelty_test_GPU.txt")
-    #data_df = pd.read_csv("./modelsOmniglotAdvStd3/resnet12_Omniglot_novelty_test.txt")
-
-    #data_df = pd.read_csv("./modelsOmniglotAdvStd4/results-5w/resnet12_Omniglot_novelty_test_GPU.txt")
-    data_df = pd.read_csv("./modelsOmniglotAdvStd4/results-5w/resnet12_Omniglot_novelty_test_CPU.txt")
     data_df = data_df.loc[data_df['Novelty'] == True]
      
     # Header
@@ -57,7 +46,75 @@ if __name__=='__main__':
     #                                        x='Shot',
     #                                        y='Std',
     #                                        color='green', ax=ax)
-    plt.title('Omniglot dataset ')
+    plt.title(title)
     plt.ylabel('Score')
-    plt.ylim(0.8, 1.0) # Omniglot
+    plt.ylim(limits) # Omniglot
     plt.show()
+
+def plotScoresVsWays(data_df, fewShotClassifier, title, limits):
+    
+    data_df_novelty = data_df.loc[data_df['Novelty'] == True]
+    data_df = data_df.loc[data_df['Novelty'] == False]
+    data_df_novelty = data_df_novelty.loc[data_df_novelty['FewShotClassifier'] == fewShotClassifier]
+    data_df = data_df.loc[data_df['FewShotClassifier'] == fewShotClassifier]
+    data_df_novelty['Way'] -= 1
+    
+    ax = plt.gca()
+    data_df.plot(kind='line',
+                x='Way',
+                y='Accuracy',
+                color='orange', ax=ax)
+    
+    data_df_novelty.plot(kind='line',
+                x='Way',
+                y='Accuracy',
+                color='red', ax=ax)
+    
+    data_df_novelty.plot(kind='line',
+                x='Way',
+                y='Precision',
+                color='blue', ax=ax)
+    
+    data_df_novelty.plot(kind='line',
+                x='Way',
+                y='Recall',
+                color='green', ax=ax)
+    
+    data_df_novelty.plot(kind='line',
+                x='Way',
+                y='F1',
+                color='black', ax=ax)
+    
+    # learned_distribution_resnet18_CUB.plot(kind='line',
+    #                                        x='Shot',
+    #                                        y='Std',
+    #                                        color='green', ax=ax)
+    #plt.title('Omniglot dataset ')
+    plt.title(title)
+    plt.ylabel('Score')
+    plt.ylim(limits) # Omniglot
+    plt.legend(["AccFewShot", "AccNovelty", "Precision", "Recall", "F1-Score"])
+    plt.show()
+    
+
+#%% MAIN
+if __name__=='__main__':
+    
+    
+    
+    #data_df = pd.read_csv("./modelsOmniglotAdvStd1/resnet12_Omniglot_novelty_test.txt")
+    #data_df = pd.read_csv("./modelsOmniglotAdvStd3/resnet12_Omniglot_novelty_test_GPU.txt")
+    #data_df = pd.read_csv("./modelsOmniglotAdvStd3/resnet12_Omniglot_novelty_test.txt")
+
+    #data_df = pd.read_csv("./modelsOmniglotAdvStd4/results-5w/resnet12_Omniglot_novelty_test_GPU.txt")
+    data_df = pd.read_csv("./modelsOmniglotAdvStd4/results-5w/resnet12_Omniglot_novelty_test_CPU.txt")
+    plotScorsVsAlpha(data_df, "Prototypical", "Omniglot Prototypical", (0.8, 1.0), n_shot=5)
+    plotScorsVsAlpha(data_df, "BD-CSPN", "Omniglot BD-CSPN", (0.8, 1.0), n_shot=5)
+    
+    fewShotClassifier = "Prototypical"
+    #fewShotClassifier = "BD-CSPN"
+
+    data_df = pd.read_csv("./modelsOmniglotAdvStd4/results-Nw_A1_0/resnet12_Omniglot_novelty_ways_test.txt")
+    plotScoresVsWays(data_df, fewShotClassifier, "Omniglot " + fewShotClassifier, (0.7, 1.0))
+    data_df = pd.read_csv("./modelsFinalPreAdv/results_Nw/resnet18_euMoths_novelty_ways_test.txt")
+    plotScoresVsWays(data_df, fewShotClassifier, "EU moths " + fewShotClassifier, (0.1, 1.0))
